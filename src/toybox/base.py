71e0657d64811d226d9307294a059cfa6b1a17a8
CHULMING <ccm2819@naver.com>
2021-11-28 17:27:44 +0900

    backup

--- src/toybox/base.py
+++ src/toybox/base.py
@@ -1,7 +1,8 @@
 import json
 import os
 from upbit.client import Upbit
-from libs import get_data
+from libs import get_data, get_std
+import numpy as np
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) 
 ENV_PATH = ROOT_DIR + "/.env.json"
@@ -32,17 +33,46 @@ class baseTrader():
         
 if __name__ == '__main__':
     bt = baseTrader()
-    print(bt.client)
-    # def get_data(client=None, market="KRW-BTC", type="minutes", from_date=[2021, 1, 1, 0, 0, 0], to_date=""):
-    data = get_data(bt.client, from_date=[2021, 11, 23, 14, 0, 0])
-    print(len(data))
-    data.reverse()
+    data = get_data(bt.client, market="KRW-SAND",from_date=[2021, 11, 25, 8, 0, 0])
+    print(f"data : {len(data)} EA")
+    
+    stds = get_std(data=data)    
+    capital = 1000000
+    stock = 0
+    buy_lines = 0
+    sell_lines = 0
     for d in data:
         open = float(d.get("opening_price", ""))
         close = float(d.get("trade_price", ""))
         high = float(d.get("high_price", ""))
         low = float(d.get("low_price", ""))
         time = d.get("candle_date_time_kst", "")
-        candle = "Plus" if close-open > 0 else "Minus"
-        result = f"{time}, {candle}, +{high-open}, -{open-low}"
-        print(result)
+        if close-open > 0:
+            close_var = (close - open) / open * 100
+            var = ((high - open) / open) * 100
+            var -= close_var
+        elif close - open < 0:
+            close_var = (close - open) / open * (-100)
+            var = ((low - open) / open) * (-100)
+            var -= close_var
+        result = f"{time}, 변동률 : {round(close_var , 2)}, 고/저점 편차 : {round(var, 2)}"
+        
+        buy_conditions = [
+            stds["Hclose_by_open"] * 100,
+            stds["Htail_by_close"] * 100,
+        ]
+        sell_conditions = [
+            stds["Lclose_by_open"] * 100,
+            stds["Ltail_by_close"] * 100,
+        ]
+        if close_var >= buy_conditions[0] and var <= buy_conditions[1]:
+            stock += 1
+            capital -= close
+            print(f"{result}, Buy {close}, stock : {stock}, capital : {capital}")
+        if close_var >= sell_conditions[0] and var <= sell_conditions[1]:
+            if stock > 0:
+                capital += (stock * close)
+                stock = 0
+                print(f"{result}, Sell {close} stock : {stock}, capital : {capital}")    
+    capital += (close * stock)
+    print(f"capital = {capital}")
--- src/toybox/libs.py
+++ src/toybox/libs.py
@@ -2,6 +2,8 @@ from upbit.client import Upbit
 import requests
 import datetime
 import json
+import time
+import numpy as np
 
 def get_data(client=None, market="KRW-BTC", type="minutes", from_date=[2021, 1, 1, 0, 0, 0], to_date=""):
     """
@@ -31,34 +33,84 @@ def get_data(client=None, market="KRW-BTC", type="minutes", from_date=[2021, 1, 
     time_now = datetime.datetime.now(datetime.timezone.utc)
     while time_cursor < time_now:
         count = 0
-        
+        temp_ret = list()
+
         while time_cursor < time_now and count < 200:
             time_cursor += time_delta
             count += 1
         
         time_str = str(time_cursor).split("+")[0].replace(":", "%3A")
-        url = f"{base_url}{type}?market={market}&to={time_str}&count={count}"    
+        url = f"{base_url}{type}?market={market}&to={time_str}&count={count}"
+        
+        while not check_can_call(client=client):
+            time.sleep(1)
+    
         response = requests.request("GET", url, headers=headers).json()
         print("call rest api !")
         
         for i in response:
+            temp_ret.append(i)
+        temp_ret.reverse()
+        for i in temp_ret:
             ret.append(i)
     
     return ret
     
+
+def check_can_call(client=None):
+    resp = client.APIKey.APIKey_info()
+    if "error" in resp["result"]:
+        return False
+    return True
+
+
+def get_std(data=list()) -> dict :
+    stds = {
+        "high_by_open": 0,
+        "high_by_close": 0,
+        "low_by_open": 0,
+        "low_by_close": 0,
+        "Hclose_by_open": 0,
+        "Lclose_by_open": 0,
+        "Htail_by_close": 0,
+        "Ltail_by_close": 0,
+    }
+    arrs = {
+        "high_by_open": np.array([]),
+        "high_by_close": np.array([]),
+        "low_by_open": np.array([]),
+        "low_by_close": np.array([]),
+        "Hclose_by_open": np.array([]),
+        "Lclose_by_open": np.array([]),
+        "Htail_by_close": np.array([]),
+        "Ltail_by_close": np.array([]),
+    }
+    for d in data:
+        open = float(d.get("opening_price", ""))
+        close = float(d.get("trade_price", ""))
+        high = float(d.get("high_price", ""))
+        low = float(d.get("low_price", ""))
+        if close >= open:
+            arrs["high_by_open"] = np.append(arrs["high_by_open"], np.array([(high - open) / open]))
+            arrs["high_by_close"] = np.append(arrs["high_by_close"], np.array([(high - close) / close]))
+            arrs["Hclose_by_open"] = np.append(arrs["Hclose_by_open"], np.array([(close - open) / open]))
+            arrs["Htail_by_close"] = np.append(arrs["Htail_by_close"], np.array([((high - open) / open) - ((close - open) / open)]))
+        else:
+            arrs["low_by_open"] = np.append(arrs["low_by_open"], np.array([(low - open) / open]))
+            arrs["low_by_close"] = np.append(arrs["low_by_close"], np.array([(low - close) / close]))
+            arrs["Lclose_by_open"] = np.append(arrs["Lclose_by_open"], np.array([(close - open) / open]))
+            arrs["Ltail_by_close"] = np.append(arrs["Ltail_by_close"], np.array([((low - open) / open) - ((close - open) / open)]))
     
+    stds["high_by_open"] = np.std(arrs["high_by_open"])
+    stds["high_by_close"] = np.std(arrs["high_by_close"])
+    stds["low_by_open"] = np.std(arrs["low_by_open"])
+    stds["low_by_close"] = np.std(arrs["low_by_close"])
+    stds["Hclose_by_open"] = np.std(arrs["Hclose_by_open"])
+    stds["Lclose_by_open"] = np.std(arrs["Lclose_by_open"])
+    stds["Htail_by_close"] = np.std(arrs["Htail_by_close"])
+    stds["Ltail_by_close"] = np.std(arrs["Ltail_by_close"])
+    
+    return stds
 
 if __name__ == "__main__":
-    # time_now = datetime.datetime.now(datetime.timezone.utc)
-    time_now = datetime.datetime(2021, 1, 1, 0, 0, 0)
-    str_time = str(time_now).split("+")[0]
-    t = str_time.replace(":", "%3A")
-    delta = datetime.timedelta(minutes=1)
-    date = datetime.datetime(2021, 1, 1, 0, 0, 0)
-    today = datetime.datetime.today()
-    
-    for i in range(200):
-        date += delta
-        print(date)
-    print(today - date)
-    print((today-date)/delta)
+    print("Hello")
