09880d0ad9dae9b10e1712b8e57d0338c69f171b
ccm2819@naver.com <chulminchae@CHULMING.local>
2022-02-18 23:02:09 +0900

    backup

--- src/toybox/account.py
+++ src/toybox/account.py
@@ -3,8 +3,8 @@ from toybox.src.toybox.base import ValuationInfo
 
 
 class Account:
-    def __init__(self) -> None:
-        self.initial_asset: float = 0  # 초기 투자금 : 처음 투자 시작한 금액
+    def __init__(self, **kwargs) -> None:
+        self.initial_asset: float = kwargs.get("asset", 0)  # 초기 투자금 : 처음 투자 시작한 금액
         self.stock: Stock = None  # 주식 정보 : 보유한 주식 정보
         self.valuation_info: ValuationInfo = ValuationInfo()  # 세부 정보 : 현 계좌의 세부 정보
         self.orderable_amount: float = 0  # 예수금 : 주문 가능 금액
